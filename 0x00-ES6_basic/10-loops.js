641425d431b6af625664e4580db6954a67d47635
Pasitik <paasitik@gmail.com>
2024-01-04 02:33:30 +0000

    10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let new arr;
-  for (let value of array) {
-    arr.push( appendString + value);
+  let arr;
+  for (const value of array) {
+    arr.push(appendString + value);
   }
 
   return arr;
